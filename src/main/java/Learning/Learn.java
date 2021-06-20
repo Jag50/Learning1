e12d736a815329be117ccf2ca659471d1d933833
jagbir.singh <84325146+Jag50@users.noreply.github.com>
2021-06-20 21:01:13 +0100

    Trying out a more in depth scanner example

--- src/main/java/Learning/Learn.java
+++ src/main/java/Learning/Learn.java
@@ -12,12 +12,13 @@ public class Learn {
         String name = scanner.nextLine();
         System.out.println("How old are you? ");
         int age = scanner.nextInt();
+        scanner.nextLine(); //I need this to mitigate pre-prompting of questions being answered
         System.out.println("What is your favourite food?");
         String food = scanner.nextLine();
 
 
         System.out.println("Hello "+name);
         System.out.println("You are "+age+" years old");
-        System.out.println("You like");
+        System.out.println("You like "+food);
     }
 }
