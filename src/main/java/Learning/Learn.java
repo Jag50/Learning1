7f334e69eb26bb53984ab7bb53a58a725dcc6d18
jagbir.singh <84325146+Jag50@users.noreply.github.com>
2021-06-20 21:13:53 +0100

    Learning java expressions

--- src/main/java/Learning/Learn.java
+++ src/main/java/Learning/Learn.java
@@ -6,19 +6,10 @@ public class Learn {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        int friends = 20;
+        friends = +1;
 
-        System.out.println("what is your name? ");
-        String name = scanner.nextLine();
-        System.out.println("How old are you? ");
-        int age = scanner.nextInt();
-        scanner.nextLine(); //I need this to mitigate pre-prompting of questions being answered
-        System.out.println("What is your favourite food?");
-        String food = scanner.nextLine();
+        System.out.println(friends);
 
-
-        System.out.println("Hello "+name);
-        System.out.println("You are "+age+" years old");
-        System.out.println("You like "+food);
     }
 }
