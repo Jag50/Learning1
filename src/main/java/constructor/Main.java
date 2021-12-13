c7f015db916b4f3fb83b0207d9d9be71483cde93
jagbir.singh <jagbir.singh@sainsburysbank.co.uk>
2021-12-13 10:27:49 +0000

    Test Friends

---
+++ src/main/java/constructor/Main.java
@@ -0,0 +1,18 @@
+package constructor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Human human1 = new Human("Rick",65,70);
+        Human human2 = new Human("Morty",16,50);
+        Human human3 = new Human("Alder", 35,85);
+
+        human1.drink();
+        human2.eat();
+
+        human1.weight();
+        human2.weight();
+
+    }
+}
