4f454b424ef2cf787a31f21040bc3aba0e3f8fab
jagbir.singh <jagbir.singh@sainsburysbank.co.uk>
2021-12-13 15:44:11 +0000

    AND, OR and NOT operators

--- src/main/java/conditionalsAndLoops/LogicalOperators.java
+++ src/main/java/conditionalsAndLoops/LogicalOperators.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class LogicalOperators {
     public static void main(String[]args){
+
+        //AND operator
         Scanner scanner = new Scanner(System.in);
         int age = scanner.nextInt();
         Scanner scanner1 = new Scanner(System.in);
@@ -14,5 +16,28 @@ public class LogicalOperators {
         } else {
             System.out.println("Not Welcome");
         }
+
+        //OR operator
+        Scanner scanner3 = new Scanner(System.in);
+        int age1 = scanner3.nextInt();
+        Scanner scanner4 = new Scanner(System.in);
+        int money1 = scanner4.nextInt();
+
+        if (age1 >= 18 || money1 >= 500){
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Not Welcome");
+        }
+
+        //NOT operator
+        Scanner scanner5 = new Scanner(System.in);
+        int age2 = scanner5.nextInt();
+
+        if (!(age2 >= 18)) {
+            System.out.println("Too Young");
+        } else {
+            System.out.println("Welcome!");
+        }
+
     }
 }
