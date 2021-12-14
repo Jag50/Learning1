8e5df995aadbbc1056f312f94eb7748fec7ec781
jagbir.singh <jagbir.singh@sainsburysbank.co.uk>
2021-12-14 11:53:30 +0000

    Loan Calculator task

---
+++ src/main/java/conditionalsAndLoops/LoanCalculator.java
@@ -0,0 +1,20 @@
+package conditionalsAndLoops;
+
+import java.util.Scanner;
+
+public class LoanCalculator {
+    public LoanCalculator() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int amount = scanner.nextInt();
+        int months = 3;
+
+        for(int i = 0; i < months; ++i) {
+            amount = amount * 90 / 100;
+            System.out.println(amount);
+        }
+
+    }
+}
